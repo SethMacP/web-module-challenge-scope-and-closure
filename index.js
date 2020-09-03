1f0399c9c692b4c66bc5ebe562300d9e2b8dc445
Seth MacPherson <macpherson.seth@gmail.com>
2020-09-02 18:28:09 -0700

    Forgot to do any commmits thus far. I am working through Task 4 on the JS assignment. Some of the readme is finished

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+
+// function personalDice(name){
+//   return function(){
+//       // generate random number between 1 and 6
+//     const newRoll = Math.floor(Math.random() * 6);
+//     console.log(`${name} rolled a ${newRoll}`)
+//   }
+// }
+
+// const dansRoll = personalDice("Dan");
+
+// const zoesRoll = personalDice("Zoe");
+
+
+// dansRoll();
+// dansRoll();
+
 // ⭐️ Example Challenge START ⭐️
 
 /**
@@ -14,11 +31,12 @@
  * Invoking `processFirstItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'foofoo'.
 */
-function processFirstItem(stringList, callback) {
-  return callback(stringList[0])
-}
+// function processFirstItem(stringList, callback) {
+//   return callback(stringList[0])
 
-// ⭐️ Example Challenge END ⭐️
+// }
+// processFirstItem();
+// // ⭐️ Example Challenge END ⭐️
 
 
 ///// M V P ///////
@@ -27,11 +45,12 @@ function processFirstItem(stringList, callback) {
  * Study the code for counter1 and counter2. Answer the questions below.
  * 
  * 1. What is the difference between counter1 and counter2?
- * 
+ *  counter1 returns additional instructions and the count stays permanent.   Counter 2 returns count++ only, but lets count be set to 0 at the start.
  * 2. Which of the two uses a closure? How can you tell?
- * 
+ *  counter1 uses closure because its modifying a variable outside of it's scope.
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
- *
+ * If you want your count to be private and only accessible from within counterMaker, use counter 1.
+ * If you want the variable count to be accessible from outside the function use counter 2.
 */
 
 // counter1 code
@@ -43,25 +62,22 @@ function counterMaker() {
 }
 
 const counter1 = counterMaker();
-
 // counter2 code
-let count = 0;
-
-function counter2() {
-  return count++;
-}
+  let count = 0;
+  function counter2() {
+    return count++;
+  }
 
 
 /* Task 2: inning() 
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
+function inning(){
+  let points = Math.floor(Math.random() * 3);
+  return points;
 }
-
+ 
 /* Task 3: finalScore()
 
 Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number of innings and and returns the final score of the game in the form of an object.
@@ -76,11 +92,54 @@ finalScore(inning, 9) might return:
 
 */ 
 
-function finalScore(/*code Here*/){
 
-  /*Code Here*/
 
+
+function finalScore(myFunc, inningsPlayed){
+
+  let score = { Home : 0, Away : 0 };
+  for (let i = 0 ; i < inningsPlayed ; i++)
+    {
+    score.Home = ( score.Home + myFunc() );
+    
+    score.Away = ( score.Away + myFunc() );
+ 
+    }
+  return score;
 }
+console.log(finalScore(inning, 1));
+
+
+
+
+//Another Fail
+// function finalScore(myFunc, inningsPlayed){
+//   const score = { Home : 0, Away : 0 };
+  
+//   /*Code Here*/
+//   function (score){
+//     score.Home = (score.Home + myFunc) * inningsPlayed;
+//     score.Away = (score.Away + myFunc) * inningsPlayed;
+//   return score;
+//   }
+// }
+// console.log(finalScore(inning, 4));
+
+
+// function inning(){
+//   let points = 0;
+//   return points = Math.floor(Math.random() * 3);
+// }
+
+
+
+
+
+
+
+
+
+
 
 /* Task 4: 
 
@@ -103,8 +162,28 @@ and returns the score at each pont in the game, like so:
 Final Score: awayTeam - homeTeam */
 
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+// let home = 0;
+// let away = 0;
+// let score = { Home : home, Away : away };
+
+
+let homeScore = [];
+let awayScore = [];
+
+
+function scoreboard( myFunc, totalRound) {
+ let homeScore = [];
+ let awayScore = [];
+  let finalScore = { Away : 0, Home : 0 };
+  for ( let i = 0 ; i < totalRound ; i++ ){
+    homeScore.push(myFunc());
+    awayScore.push(myFunc());
+    return 'Inning: ${totalRound}'
+    }
+    console.log(awayScore);
+    console.log(homeScore);
+  
 }
-
-
+scoreboard(inning, 9 )
+console.log(awayScore);
+console.log(homeScore);
